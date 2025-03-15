41e920772f00376a0bdd8edacc0dec35b38149bd
Lala-Lu25 <makesmesmile105@gmail.com>
2025-03-15 20:53:22 +0800

    js and others

--- website/js/script.js
+++ website/js/script.js
@@ -2,18 +2,39 @@
 let username
 let password
 
+// menu
+const menu = document.getElementsByClassName("usermenu")
+
+// 
+let optn1 = document.getElementById('optn1')
+let optn2 = document.getElementById('optn2')
+
 // profile log in
 function checkprofile() {
 
-    // create element
-    const container = document.createElement("div")
-    
-
-    // check
-    if (username && password) {
-        // html
+    // show
+    if (menu.style.display === "block") {
+        menu.style.display = "none"
 
     } else {
+        menu.style.display = "block" 
 
+        // check
+        if (username && password) {
+            // my profile and log out
+            optn1.innerHTML = "My Profile"
+            optn2.innerHTML = "Log Out"
+
+            // show profile and log out
+
+            // style
+
+        } else {
+            // sign up and login
+
+            // show profile and log out
+
+            // style
+        }
     }
 }
--- website/nav.php
+++ website/nav.php
@@ -18,6 +18,12 @@
                 </li>
             </ul>
         </nav>
+        <div class="usermenu">
+            <div class="container-menu">
+                <a href="" id="optn1"></a>
+                <a href="" id="optn2"></a>
+            </div>
+        </div>
         <script src="script.js"></script>
     </body> 
 </html>
