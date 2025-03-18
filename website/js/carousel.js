e06cccd7353f438079b62e29ffdfe784c4ae8d54
yambaolr <yambaolorinrobelle@gmail.com>
2025-03-18 22:36:45 +0800

    adjustment css and carousel js

--- website/js/carousel.js
+++ website/js/carousel.js
@@ -1,9 +1,11 @@
 function nextSlide(type) {
     let container = document.getElementById(type);
-    container.scrollLeft += 250;
+    let cardWidth = container.querySelector(".car-card").offsetWidth + 50;
+    container.scrollLeft += cardWidth;
 }
 
 function prevSlide(type) {
     let container = document.getElementById(type);
-    container.scrollLeft -= 250;
+    let cardWidth = container.querySelector(".car-card").offsetWidth + 50;
+    container.scrollLeft -= cardWidth;
 }
