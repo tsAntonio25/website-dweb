bee22d248fa1dc9b2e4c581053b6bc61fa8b1894
Lala-Lu25 <makesmesmile105@gmail.com>
2025-03-25 16:09:45 +0800

    sign up page
    di pa natest

--- website/signup.php
+++ website/signup.php
@@ -14,7 +14,7 @@
     <section class = "signup-section">
         <h1>Sign Up</h1>
 
-        <form action="admin/operations/create.php" method="post">
+        <form action="admin/create.php" method="post">
             <div class="form-group">
                 <h3>Personal Information</h3>
                 <div class="row">
