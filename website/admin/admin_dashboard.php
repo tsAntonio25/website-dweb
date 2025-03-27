433e9078b1e7d1bac40e083fb0eb0a8708bb7509
yambaolr <yambaolorinrobelle@gmail.com>
2025-03-27 23:56:44 +0800

    <

--- website/admin/admin_dashboard.php
+++ website/admin/admin_dashboard.php
@@ -17,7 +17,7 @@ include 'connectivity.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin - Dashboard</title>
     <link rel="stylesheet" href="css\styles.css">
-    <<link rel="apple-touch-icon" sizes="180x180" href="../assets/logo.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/logo.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../assets/logo.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../assets/logo.png">
 </head>
